f8279acb2a27b194f76ce65f088eb49633e728e6
Swarnalakshmi G B <lakshmiswarna041@gmail.com>
2022-06-27 11:12:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ app.get('/', function (req, res) {
 res.send('Hello Welcome to the Nodejs app')
 })
 app.listen(3030, function () {
-console.log('app listening on port 3030!')
+console.log('app listening on port 3030!!')
 })
